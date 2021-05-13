7487d47c4fc2476a8fbfbdc6eb08cec98ed52259
Chris Pressland <mail@cpressland.io>
2021-05-13 15:31:53 +0100

    Added CAA records

--- dns.tf
+++ dns.tf
@@ -20,6 +20,31 @@ resource "azurerm_dns_a_record" "api_record" {
     records = [var.firewall.public_ip]
 }
 
+resource "azurerm_dns_caa_record" "api_record" {
+    name = "api.${var.cluster_name}.uksouth"
+    zone_name = var.public_dns["bink_sh"].dns_zone_name
+    resource_group_name = var.public_dns["bink_sh"].resource_group_name
+    ttl = 300
+
+    record {
+        flags = 0
+        tag = "issue"
+        value = azurerm_dns_a_record.api_record.fqdn
+    }
+
+    record {
+        flags = 0
+        tag = "issuewild"
+        value = ";"
+    }
+
+    record {
+        flags = 0
+        tag = "iodef"
+        value = "mailto:devops@bink.com"
+    }
+}
+
 resource "azurerm_dns_a_record" "web_record" {
     provider = azurerm.core
 
@@ -28,6 +53,31 @@ resource "azurerm_dns_a_record" "web_record" {
     resource_group_name = var.public_dns["bink_sh"].resource_group_name
     ttl = 300
     records = [var.firewall.public_ip]
+}
+
+resource "azurerm_dns_caa_record" "web_record" {
+    name = "web.${var.cluster_name}.uksouth"
+    zone_name = var.public_dns["bink_sh"].dns_zone_name
+    resource_group_name = var.public_dns["bink_sh"].resource_group_name
+    ttl = 300
+
+    record {
+        flags = 0
+        tag = "issue"
+        value = azurerm_dns_a_record.web_record.fqdn
+    }
+
+    record {
+        flags = 0
+        tag = "issuewild"
+        value = ";"
+    }
+
+    record {
+        flags = 0
+        tag = "iodef"
+        value = "mailto:devops@bink.com"
+    }
 }
 
 resource "azurerm_dns_a_record" "policies_record" {
@@ -40,6 +90,31 @@ resource "azurerm_dns_a_record" "policies_record" {
     records = [var.firewall.public_ip]
 }
 
+resource "azurerm_dns_caa_record" "policies_record" {
+    name = "policies.${var.cluster_name}.uksouth"
+    zone_name = var.public_dns["bink_sh"].dns_zone_name
+    resource_group_name = var.public_dns["bink_sh"].resource_group_name
+    ttl = 300
+
+    record {
+        flags = 0
+        tag = "issue"
+        value = azurerm_dns_a_record.policies_record.fqdn
+    }
+
+    record {
+        flags = 0
+        tag = "issuewild"
+        value = ";"
+    }
+
+    record {
+        flags = 0
+        tag = "iodef"
+        value = "mailto:devops@bink.com"
+    }
+}
+
 resource "azurerm_dns_a_record" "link_record" {
     provider = azurerm.core
 
@@ -50,6 +125,31 @@ resource "azurerm_dns_a_record" "link_record" {
     records = [var.firewall.public_ip]
 }
 
+resource "azurerm_dns_caa_record" "link_record" {
+    name = "link.${var.cluster_name}.uksouth"
+    zone_name = var.public_dns["bink_sh"].dns_zone_name
+    resource_group_name = var.public_dns["bink_sh"].resource_group_name
+    ttl = 300
+
+    record {
+        flags = 0
+        tag = "issue"
+        value = azurerm_dns_a_record.link_record.fqdn
+    }
+
+    record {
+        flags = 0
+        tag = "issuewild"
+        value = ";"
+    }
+
+    record {
+        flags = 0
+        tag = "iodef"
+        value = "mailto:devops@bink.com"
+    }
+}
+
 resource "azurerm_dns_a_record" "base_record" {
     provider = azurerm.core
 
@@ -58,4 +158,29 @@ resource "azurerm_dns_a_record" "base_record" {
     resource_group_name = var.public_dns["bink_sh"].resource_group_name
     ttl = 300
     records = [var.firewall.public_ip]
+}
+
+resource "azurerm_dns_caa_record" "base_record" {
+    name = "${var.cluster_name}.uksouth"
+    zone_name = var.public_dns["bink_sh"].dns_zone_name
+    resource_group_name = var.public_dns["bink_sh"].resource_group_name
+    ttl = 300
+
+    record {
+        flags = 0
+        tag = "issue"
+        value = azurerm_dns_a_record.base_record.fqdn
+    }
+
+    record {
+        flags = 0
+        tag = "issuewild"
+        value = ";"
+    }
+
+    record {
+        flags = 0
+        tag = "iodef"
+        value = "mailto:devops@bink.com"
+    }
 }
