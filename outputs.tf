8e7b38d5bd601f8e81ffe8b2d00ac21b1f868dc8
Chris Pressland <mail@cpressland.io>
2021-05-13 14:34:45 +0100

    improved 'link' dns record

--- outputs.tf
+++ outputs.tf
@@ -16,6 +16,15 @@ output "frontdoor_backend_policies_pool" {
     }
 }
 
+output "frontdoor_backend_link_pool" {
+    value = {
+        host_header = trimsuffix(azurerm_dns_a_record.link_record.fqdn, ".")
+        address = trimsuffix(azurerm_dns_a_record.link_record.fqdn, ".")
+        http_port = var.firewall.ingress_http
+        https_port = var.firewall.ingress_https
+    }
+}
+
 output "worker_subnet" {
     value = azurerm_subnet.worker.id
 }
