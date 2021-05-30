a4d8ea522e55fca5cbb2d7a9bbde2a11786c45e4
Chris Pressland <cpressland@bink.com>
2021-05-30 13:59:40 +0000

    Added Subscription Peers, probably not the best implementation

--- peer.tf
+++ peer.tf
@@ -1,4 +1,4 @@
-# Source peers
+# Core Source peers
 resource "azurerm_virtual_network_peering" "peer" {
     for_each = var.peers
 
@@ -10,11 +10,35 @@ resource "azurerm_virtual_network_peering" "peer" {
     allow_forwarded_traffic = true
 }
 
-# Remote peers
+# Core Remote peers
 resource "azurerm_virtual_network_peering" "remote_peer" {
     for_each = var.peers
 
     provider = azurerm.core
+
+    name = "local-to-${azurerm_resource_group.rg.name}"
+    resource_group_name = each.value["resource_group_name"]
+    virtual_network_name = each.value["vnet_name"]
+    remote_virtual_network_id = azurerm_virtual_network.vnet.id
+    allow_virtual_network_access = true
+    allow_forwarded_traffic = true
+}
+
+# Subscription Source Peers
+resource "azurerm_virtual_network_peering" "sub_peer" {
+    for_each = var.subscription_peers
+
+    name = "local-to-${each.key}"
+    resource_group_name = azurerm_resource_group.rg.name
+    virtual_network_name = azurerm_virtual_network.vnet.name
+    remote_virtual_network_id = each.value["vnet_id"]
+    allow_virtual_network_access = true
+    allow_forwarded_traffic = true
+}
+
+# Subscription Remote peers
+resource "azurerm_virtual_network_peering" "sub_remote_peer" {
+    for_each = var.subscription_peers
 
     name = "local-to-${azurerm_resource_group.rg.name}"
     resource_group_name = each.value["resource_group_name"]
--- vars.tf
+++ vars.tf
@@ -69,6 +69,13 @@ variable peers { type = map(object({
     vnet_name = string
     resource_group_name = string
 })) }
+
+variable subscription_peers { type = map(object({
+    vnet_id = string
+    vnet_name = string
+    resource_group_name = string
+})) }
+
 # variable private_dns_link_bink_host { type = map(string) }
 # variable "private_dns_link_bink_sh" {}
 
