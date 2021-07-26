234c1bb93bc43cc2ba458b2c5259e4e2b3042cad
Terry Cain <tcain@bink.com>
2021-07-26 13:11:11 +0100

    Added TF 1.0 metadata

--- versions.tf
+++ versions.tf
@@ -2,6 +2,8 @@ terraform {
     required_providers {
         azurerm = {
             source = "hashicorp/azurerm"
+            version = ">= 2.69.0"
+            configuration_aliases = [ azurerm.core ]
         }
         chef = {
             source = "terrycain/chef"
